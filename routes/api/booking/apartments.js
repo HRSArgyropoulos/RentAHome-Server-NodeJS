774b3d01fb350ce34829d44c72783638c1fedf63
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-09-18 16:35:05 +0300

    provide fake delay in fetching results

--- routes/api/booking/apartments.js
+++ routes/api/booking/apartments.js
@@ -3,9 +3,11 @@ const {
   getApartmentsInitialSearch,
 } = require('../../../db/actions/apartment');
 
-router.get('/', async (req, res, next) => {
-  const apartments = await getApartmentsInitialSearch(req.query);
-  return res.status(200).json({ apartments });
+router.get('/', (req, res) => {
+  setTimeout(async () => {
+    const apartments = await getApartmentsInitialSearch(req.query);
+    return res.status(200).json({ apartments });
+  }, 4 * 1000); // fake server delay
 });
 
 module.exports = router;
